6088e7e7c698505d13b938ae9507e71097beae1a
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-06-22 02:42:17 +0700

    Add simple token authentication

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,51 +23,61 @@ class UsersTableSeeder extends Seeder
                 'name' => 'Jack Sparrow',
                 'email' => 'jack@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'jack',
             ],
             [
                 'name' => 'Davy Jones',
                 'email' => 'davy@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'davy',
             ],
             [
                 'name' => 'Will Turner',
                 'email' => 'will@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'will',
             ],
             [
                 'name' => 'Hector Barbossa',
                 'email' => 'hector@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'hector',
             ],
             [
                 'name' => 'Elizabeth Swan',
                 'email' => 'elizabeth@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'elizabeth',
             ],
             [
                 'name' => 'Tia Dalma',
                 'email' => 'tia@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'tia',
             ],
             [
                 'name' => 'James Norrington',
                 'email' => 'james@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'james',
             ],
             [
                 'name' => 'Joshamee Gibbs',
                 'email' => 'joshamee@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'joshamee',
             ],
             [
                 'name' => 'Cutler Beckett',
                 'email' => 'cutler@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'cutler',
             ],
             [
                 'name' => 'Edward Teach',
                 'email' => 'edward@gmail.com',
                 'password' => bcrypt('secret'),
+                'auth_token' => 'edward',
             ]
         ];
     }
