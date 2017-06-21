4f0bc6f139431ef39ccb0ebbf2f3b4edf77d4004
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-06-22 01:34:47 +0700

    Install Laravel API Doc generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Mpociot\ApiDoc\ApiDocGeneratorServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment() !== 'production') {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
+            $this->app->register(ApiDocGeneratorServiceProvider::class);
         }
     }
 }
