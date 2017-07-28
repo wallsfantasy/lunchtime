c9cfd609d51e86a510c9f681403ca9fa5290e767
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-07-28 22:48:53 +0700

    Removes get my members and clean leftover dumps

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Model\Cycle\Cycle;
-use App\Model\Cycle\Member;
 use App\Model\User\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -25,35 +23,22 @@ class UserController extends Controller
     }
 
     /**
-     * Get my Members
+     * Get my Cycles
      *
      * @param Request $request
      *
-     * @return Collection|Member[]
+     * @return Collection|Cycle[]
      */
-    public function getMyMembers(Request $request)
-    {
-        return Member::where(['user_id' => $request->user()->id])->get();
-    }
-
     public function getMyCycles(Request $request)
     {
-//        $result = DB::table('cycles')
-//            ->join('cycle_members', 'cycles.id', '=', 'cycle_members.cycle_id')
-//            ->select('cycles.*')
-//            ->where('cycle_members.user_id', '=', $request->user()->id)
-//            ->get();
-        DB::connection()->enableQueryLog();
-
         $userId = $request->user()->id;
+
         $result = Cycle::whereHas('members', function ($query) use ($userId) {
             $query->where('user_id', '=', $userId);
         })
             ->with('members')
             ->get();
 
-        $queries = DB::getQueryLog();
-//        dd($queries);
         return $result;
     }
 }
