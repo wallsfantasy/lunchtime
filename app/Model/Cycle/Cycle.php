e3c15495d7d4108ae97b648628f0c73c642da987
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-07-30 21:45:20 +0700

    Type hint member property in Cycle

--- app/Model/Cycle/Cycle.php
+++ app/Model/Cycle/Cycle.php
@@ -2,14 +2,16 @@
 
 namespace App\Model\Cycle;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * @property int    $id
- * @property string $name
- * @property string $propose_until
- * @property string $lunchtime
+ * @property int                 $id
+ * @property string              $name
+ * @property string              $propose_until
+ * @property string              $lunchtime
+ * @property Collection|Member[] $members
  * @mixin \Eloquent
  */
 class Cycle extends Model
