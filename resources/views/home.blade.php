042fdd2c4ba48e0bb4edd799c716169cb43482cd
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-07-31 23:56:06 +0700

    Add basic sidebar to base layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="row">
+        <div class="panel panel-default">
+            <div class="panel-heading">Your Cycles</div>
+            <div class="panel-body">
+                <ul>
+                    @forelse($cycles as $cycle)
+                        <li>{{ $cycle->name }}</li>
+                    @empty
+                        <li>You haven't join any cycle. Click here to add some!</li>
+                    @endforelse
+                </ul>
+            </div>
+        </div>
+    </div>
+    @forelse($userProposesByCycle as $cycleName => $userPropose)
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Your Cycles</div>
-                    <div class="panel-body">
-                        <ul>
-                            @forelse($cycles as $cycle)
-                                <li>{{ $cycle->name }}</li>
-                            @empty
-                                <li>You haven't join any cycle. Click here to add some!</li>
-                            @endforelse
-                        </ul>
-                    </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    {{ $cycleName }}
+                </div>
+                <div class="panel-body">
+                    <ul>
+                        @foreach($userPropose as $userName => $restaurantName)
+                            @if($restaurantName !== null)
+                                <li>{{ $userName }} {{ $restaurantName }}</li>
+                            @endif
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
-        @forelse($userProposesByCycle as $cycleName => $userPropose)
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            {{ $cycleName }}
-                        </div>
-                        <div class="panel-body">
-                            <ul>
-                                @foreach($userPropose as $userName => $restaurantName)
-                                    @if($restaurantName !== null)
-                                        <li>{{ $userName }} {{ $restaurantName }}</li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @empty
-        @endforelse
-    </div>
+    @empty
+    @endforelse
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,67 +14,78 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
             </div>
-        </nav>
 
-        @yield('content')
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-1 col-sm-2 col-md-2 col-lg-2 sidebar">
+                @include('layouts.sidebar')
+            </div>
+            <div class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-1 col-md-8 col-lg-8 col-lg-offset-1 main">
+                @yield('content')
+            </div>
+        </div>
     </div>
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
