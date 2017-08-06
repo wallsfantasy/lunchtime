0cfba58c11d6fa4fcb75c45dd0f0641c1689182d
Tee Tanawatanakul <wallsfantasy@gmail.com>
2017-08-06 22:59:17 +0700

    Add test for restaurant page

---
+++ tests/Feature/Restaurant/Web/ShowRestaurantPageTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Restaurant\Web;
+
+use App\Model\Restaurant\Restaurant;
+use App\Model\User\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ShowRestaurantPageTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testGetRestaurantSuccess()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        /** @var Restaurant $restaurant */
+        $restaurant = factory(Restaurant::class)->create(['name' => 'Amino Restaurant']);
+
+        $this->actingAs($user);
+        $response = $this->get('/restaurant');
+
+        $response->assertSeeText($restaurant->name);
+    }
+
+    public function testGetRestaurantPaginatedSuccess()
+    {
+        // tbd
+    }
+}
